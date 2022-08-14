d8b8016bff6d37e29c1841dd888931af105a4621
ANISH <anish.vilayil.s@gmail.com>
2022-08-14 12:19:35 +0530

    Update training.py

--- training.py
+++ training.py
@@ -2,8 +2,6 @@ import warnings
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
